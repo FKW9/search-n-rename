f5c6b0913afa21a1d4dc084dfe95e8680df45163
mitsubishievo99 <florian.weissensteiner@gmail.com>
2021-11-27 19:34:57 +0100

    added batch scripts to create .exe

---
+++ build_executable_cmd.bat
@@ -0,0 +1,3 @@
+CALL pyinstaller -F -n "rename_cmd" --distpath executable rename_cmd.py
+
+cmd \k
---
+++ build_executable_qt5.bat
@@ -0,0 +1,3 @@
+CALL pyinstaller -w -F -n "rename_qt5" --distpath executable rename_qt5.py
+
+cmd \k
